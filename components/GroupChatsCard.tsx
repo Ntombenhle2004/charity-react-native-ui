b743948973a18f2559dbe04ab42062fb354ccdd6
ntorh <ngcobontombenhle434@gmail.com>
2025-12-09 14:38:45 +0200

    created ui for group chats'

--- components/GroupChatsCard.tsx
+++ components/GroupChatsCard.tsx
@@ -1,11 +1,12 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image,} from "react-native";
+import RecentChatItem from "./RecentChatsItem";
 
 export default function GroupChatCard() {
   return (
     <View style={styles.card}>
       <Text style={styles.title}>Group Chat</Text>
 
-      <View style={styles.row}>
+      {/* <View style={styles.row}>
         <Image
           source={require("../assets/images/woman-floral-traditional-costume_1308-176159-removebg-preview.png")}
           style={styles.groupAvatar}
@@ -15,12 +16,23 @@ export default function GroupChatCard() {
           <Text style={styles.gText}>Kate and Ann are typing...</Text>
         </View>
 
-        <Text style={styles.time}>12:32</Text>
+        <View style={styles.rightSide}>
+          <Text style={styles.time}>12:32</Text>
 
-        <View style={styles.counter}>
-          <Text style={styles.counterText}>6</Text>
+          <View style={styles.counter}>
+            <Text style={styles.counterText}>6</Text>
+          </View>
         </View>
-      </View>
+      </View> */}
+     
+      <RecentChatItem
+        name="Kevin’s BP"
+        message="Kate and Ann are typing..."
+        time="11:34"
+        image={require("../assets/images/smiling-redhaired-boy-illustration_1308-176664-removebg-preview.png")}
+        count={6}
+        avatarBackgroundColor="#D7B8F2"
+      />
     </View>
   );
 }
@@ -29,7 +41,7 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: "white",
     borderRadius: 40,
-    padding: 20,
+    padding: 36,
     marginTop: 5,
   },
   title: { fontSize: 20, fontWeight: "700", marginBottom: 10 },
@@ -37,12 +49,15 @@ const styles = StyleSheet.create({
   groupAvatar: { width: 55, height: 55, borderRadius: 40 },
   gName: { fontSize: 16, fontWeight: "700" },
   gText: { fontSize: 14, color: "#777" },
-  time: { marginRight: 10, color: "#777" },
+  time: { color: "#777", marginBottom: 4 },
   counter: {
     backgroundColor: "#d3ea92",
-    paddingHorizontal: 10,
-    paddingVertical: 4,
-    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "black",
+    paddingHorizontal: 7,
+    paddingVertical: 1.4,
+    borderRadius: 18,
   },
   counterText: { fontWeight: "700" },
+  rightSide: { alignItems: "flex-end" },
 });
